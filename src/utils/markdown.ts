82f551f30a45e79e198cbf1024c569d98a98ff19
LovlyBoi <728881636@qq.com>
2022-09-15 13:24:29 +0800

    feat(utils/markdown): highlightjs & marked/slugger 生成id

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,5 +1,9 @@
-import { marked } from "marked";
-import type { ParsedHtml, Outline } from '../types'
+// import { readFile } from "fs/promises";
+// import { resolve } from "path";
+import { marked, Slugger } from "marked";
+
+import hljs from "highlight.js";
+import type { ParsedHtml, Outline } from "../types";
 
 export function parseMarkDown(markdownString: string | Buffer): ParsedHtml {
   markdownString =
@@ -7,20 +11,35 @@ export function parseMarkDown(markdownString: string | Buffer): ParsedHtml {
       ? markdownString
       : markdownString.toString();
   const outline: Outline = [];
+  // slugger 是marked内部生成id的工具，使用同样的工具获得相同的id
+  const slugger = new Slugger();
   const html = marked.parse(markdownString, {
     headerIds: true,
     walkTokens(token) {
       if (token.type === "heading") {
         outline.push({
-          anchor: "#" + token.text,
+          anchor: "#" + slugger.slug(token.text),
           depth: token.depth,
           title: token.text,
         });
       }
     },
+    // langPrefix: 'marked-',
+    // 直接在服务端生成 highlight
+    highlight(code, lang) {
+      if (lang === "vue") lang = "html";
+      const html: string = hljs.highlight(code, { language: lang }).value;
+      return html;
+    },
   });
+
   return {
     html,
     outline,
   };
 }
+
+// readFile(resolve(__dirname, "./vite")).then((val) => {
+//   console.log(parseMarkDown(val))
+//   // parseMarkDown(val)
+// })
