83ef6b01ec397c293df85e366faa287ac35ee29e
LovlyBoi <728881636@qq.com>
2022-10-14 21:32:28 +0800

    fix(dao/statement): 合并时错误

--- src/dao/statement.ts
+++ src/dao/statement.ts
@@ -122,15 +122,6 @@ SET
 WHERE
   blogs.nanoid = ?;`;
 
-const UPDATE_BLOG_UPDATE_DATE = `
-/* 更新博客更新时间 */
-UPDATE
-  blogs
-SET
-  update_date = CURRENT_TIMESTAMP
-WHERE
-  blogs.nanoid = ?;`
-
 export {
   INIT_DATABASE,
   STORE_BLOGS,
